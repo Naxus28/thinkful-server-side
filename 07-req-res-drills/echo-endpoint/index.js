22f6fe132f34b055500463461e75809a570475ae
Gabriel Ferraz <gabrielferraz27@gmail.com>
2018-05-24 11:53:19 -0400

    adds check for query strings presence before adding to response obj

--- 07-req-res-drills/echo-endpoint/index.js
+++ 07-req-res-drills/echo-endpoint/index.js
@@ -5,12 +5,18 @@ const PORT = process.env.PORT || 8080;
 
 // req.get('host') gets the domain and the port if it is part of the url
 // req.hostname is the domain
-app.get('/:what', (req, res) => {
-  res.json({
+app.get('/echo/:what', (req, res) => {
+  let query = req.query;
+  let response = {
     hostname: `${req.protocol}://${req.get('host')}${req.originalUrl}`,
-    query: req.query,
     params: req.params
-  });
+  };
+
+  if (Object.keys(query).length) {
+    response.query = query;
+  } 
+
+  res.json(response);
 });
 
 app.listen(PORT, () => console.log(`Serving app on http://localhost:${PORT}`));
